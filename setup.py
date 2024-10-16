7bd2887e3a70fedc96beddc899a8f702ece362c7
Keshav Kumar <keshavkmr48@users.noreply.github.com>
2024-10-16 12:58:00 +0530

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import pkg_resources
+
+def get_installed_packages():
+    """
+    Get a list of installed packages and their versions.
+    Returns a list of formatted strings that can be used in a setup.py dependencies list.
+    """
+    installed_packages = pkg_resources.working_set
+    dependencies = []
+    
+    for package in installed_packages:
+        package_name = package.key
+        version = package.version
+        dependencies.append(f"{package_name}=={version}")
+    
+    return dependencies
+
+dependencies = get_installed_packages()
+    
+
+
+
+setup(
+    name="LLMOps",  # The name of your package
+    version="0.1.0",  # Initial version
+    packages=find_packages(),  # Automatically find all the packages in the directory
+    install_requires=dependencies,
+    include_package_data=True,  # To include any non-Python files specified in MANIFEST.in
+    description="A package for LLMOps related tasks",  # Short description
+    author="Your Name",  # Your name or the team's name
+    author_email="your.email@example.com",  # Your contact email
+    url="https://your-project-repository-url.com",  # URL of the project repository
+    classifiers=[
+        "Programming Language :: Python :: 3",  # Specify Python versions supported
+        "License :: OSI Approved :: MIT License",  # License type
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',  # Python version compatibility
+)
